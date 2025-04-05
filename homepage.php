639bce26139781caf602c90d32a3da8ca49c4518
benjamin-voca <benjaminvocaa@gmail.com>
2025-04-05 13:24:30 +0200

    test commit

--- homepage.php
+++ homepage.php
@@ -411,7 +411,7 @@
     <footer>
         <div class="row">
             <div class="col-12">
-                <p>&copy; <span><?php echo date("Y-m-d"); ?></span> <em>AlgoVerse Academy. All rights reserved.</em></p>
+                <p>&copy; <span><?php echo date("Y-m-d"); ?></span > <em>AlgoVerse Academy. All rights reserved.</em></p>
             </div>
         </div>
     </footer>
