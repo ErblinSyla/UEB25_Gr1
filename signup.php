bbd2bba67729cced9fa313688042be3609d70436
bashkimhashani <bashkim.hashani@student.uni-pr.edu>
2025-05-25 18:56:29 +0200

    sing-up fixed

--- signup.php
+++ signup.php
@@ -53,6 +53,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up - AlgoVerse Academy</title>
+    <link rel="stylesheet" href="style/signup.css">
     <link rel="stylesheet" href="style_light/signup.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
